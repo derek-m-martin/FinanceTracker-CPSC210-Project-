b5b9fc3361d3d557ae0d6f713463f3a1ee1f3ff5
derek-m-martin <derekmartin1005@gmail.com>
2024-10-01 16:05:35 -0700

    Added Tests for Transaction Class

--- src/test/model/TransactionTest.java
+++ src/test/model/TransactionTest.java
@@ -1,5 +1,51 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TransactionTest {
 
+    private Transaction testTransaction;
+    private Category testCategory1;
+    private Category testCategory2;
+    private List<Transaction> transactions;
+
+    @BeforeEach
+    void runBefore() {
+        transactions = new ArrayList<>();
+        testCategory1 = new Category("TestCategory1", 500, transactions);
+        testCategory2 = new Category("TestCategory2", 1250, transactions);
+        testTransaction = new Transaction(100, testCategory1, "TestDescription");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(100, testTransaction.getAmount());
+        assertEquals("TestDescription", testTransaction.getDescription());
+        assertEquals(null, testTransaction.getCategory());
+    }
+
+    @Test 
+    void testSetAmount() {
+        testTransaction.setAmount(500);
+        assertEquals(500, testTransaction.getAmount());
+    }
+
+    @Test
+    void testSetDescription() {
+        testTransaction.setDescription("Tested");
+        assertEquals("Tested", testTransaction.getDescription());
+    }
+
+    @Test
+    void testMoveTransaction() {
+        assertEquals(testCategory1, testTransaction.getCategory());
+        testTransaction.moveTransaction(testCategory2);
+        assertEquals(testCategory2, testTransaction.getCategory());
+    }
 }
