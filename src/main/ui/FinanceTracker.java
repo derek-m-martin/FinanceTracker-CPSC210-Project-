595a13229fce93c57435b8a815e55763551ed479
derek-m-martin <derekmartin1005@gmail.com>
2024-10-04 16:03:59 -0700

    Added Specification for FinanceTracker UI class

--- src/main/ui/FinanceTracker.java
+++ src/main/ui/FinanceTracker.java
@@ -1,5 +1,65 @@
 package ui;
 
+import java.util.Locale.Category;
+
 public class FinanceTracker {
 
+    // create four generic spending categories (more can be made by the user)
+    private Category food;
+    private Category transportation;
+    private Category entertainment;
+    private Category housing;
+
+    // EFFECTS: starts the financetracker console application
+    public FinanceTracker() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes input and keeps the program running until quit
+    private void startTracker() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: manages user inputs
+    private void processInput() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initalizes the generic categories
+    private void init() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts for transaction details and then creates the transaction accordingly
+    private void handleAddTransaction() {
+        // stub
+    }
+
+    // EFFECTS: displays transactions, can be filtered by date/category
+    private void handleViewTransactions() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: allows user to select categories and set the budget as desired
+    private void handleSetBudget() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: allows user to select specific transactions and edit information about them
+    // or delete the transaction entirely
+    private void handleEditTransaction() {
+        // stub
+    }
+
+    // EFFECTS: shows all the categories and current available budgets remaining
+    private void showCategories() {
+        // stub
+    }
+    
 }
