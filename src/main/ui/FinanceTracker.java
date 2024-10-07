2503107f8b2f9e86d2ac08777dbbb442122c5860
derek-m-martin <derekmartin1005@gmail.com>
2024-10-06 21:18:16 -0700

    Updated the FinanceTracker class to be compatible with the new way in which HashMaps are instantiated for Category objects

--- src/main/ui/FinanceTracker.java
+++ src/main/ui/FinanceTracker.java
@@ -68,10 +68,10 @@ public class FinanceTracker {
     // MODIFIES: this
     // EFFECTS: initalizes the generic categories and input scanner
     private void initilize() {
-        categories.put("food", new Category("Food", 100, null));
-        categories.put("transportation", new Category("Transportation", 100, null));
-        categories.put("entertainment", new Category("Entertainment", 100, null));
-        categories.put("housing", new Category("Housing", 100, null));
+        categories.put("food", new Category("FOOD", 100));
+        categories.put("transportation", new Category("TRANSPORTATION", 100));
+        categories.put("entertainment", new Category("ENTERTAINMENT", 100));
+        categories.put("housing", new Category("HOUSING", 100));
 
         input = new Scanner(System.in);
     }
@@ -80,27 +80,39 @@ public class FinanceTracker {
     // EFFECTS: prompts for transaction details and then creates the transaction accordingly
     private void handleAddTransaction() {
         int amount;
-        Category category;
+        Category category = null;
         String desc;
         System.out.println("\n\t Please enter the transaction amount:");
         amount = Integer.parseInt(input.next());
-        System.out.println("\n\t Please specify a category from below for the transaction:");
-        showCategories();
-        category = findCategory(input.next());
-        System.out.println("\n\t OPTIONAL - enter a brief transaction description, say 'skip' if not needed:");
-        desc = input.next();
-        if (desc.equalsIgnoreCase("skip")) {
-            desc = "";
+        System.out.println("\n\t Please enter an existing or new category for the transaction:");
+        String holder = input.next();
+        category = findCategory(holder);
+        if (category == null) {
+            System.out.println("\n\t A category by that name was not located, would you like to create a new one with that name? (yes or no)");
+            if (input.next().equalsIgnoreCase("yes")) {
+                category = new Category(holder.toUpperCase(), 100);
+                categories.put(holder, category);
+            }
         }
-        new Transaction(amount, category, desc);
+        if (category != null) {
+            System.out.println("\n\t OPTIONAL - enter a brief transaction description, say 'skip' if not needed:");
+            desc = input.next();
+            if (desc.equalsIgnoreCase("skip")) {
+                desc = "";
+            }
+            Transaction temp = new Transaction(amount, category);
+            temp.setDescription(desc);
+            category.getTransactions().put(temp.getId(), temp);
+        }
     }
+    
 
     // REQUIRES: a non-empty string
     // EFFECTS: finds the category object from given string and returns the category object,
     // if no such category exists then it will return null
     public Category findCategory(String toFind) {
         for (HashMap.Entry<String, Category> entry : categories.entrySet()) {
-            if (entry.getKey().equals(toFind)) {
+            if (entry.getKey().equalsIgnoreCase(toFind)) {
                 return entry.getValue();
             }
         }
