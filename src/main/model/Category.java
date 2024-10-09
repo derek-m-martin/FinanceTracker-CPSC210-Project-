28d3dc923ccfbdf18d2c38a43eb9812e5ee6f3fa
derek-m-martin <derekmartin1005@gmail.com>
2024-10-09 16:18:29 -0700

    Double checked formatting and made changes to ensure it passes checkstyle rules

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -2,56 +2,51 @@ package model;
 
 import java.util.HashMap;
 
-// represents a category within the finance tracker which has a name, allocated budget amount, and a 
-// corresponding hashmap of all the transactions that have been assigned to the category
+// Represents a category within the finance tracker with a name, budget, and transactions
 
 public class Category {
-    
+
     private String name;
     private int budget;
     private HashMap<Integer, Transaction> transactions;
 
-    // REQUIRES: catName must not be an empty string, catBudget must be > 0, catTransactions must be an empty list
-    // EFFECTS: category name is set to catName, category budget is set to catBudget, 
-    //          and an empty transaction list is initialized for the category
+    // REQUIRES: catName is not empty, catBudget > 0
+    // EFFECTS: initializes a category with the given name and budget; transactions
+    // are initialized empty
     public Category(String catName, int catBudget) {
         this.name = catName;
         this.budget = catBudget;
         this.transactions = new HashMap<>();
     }
-    
 
-    // REQUIRES: newName must not be an empty string
+    // REQUIRES: newName is not empty
     // MODIFIES: this
-    // EFFECTS: sets the category name equal to newName
+    // EFFECTS: sets the category name
     public void setName(String newName) {
         this.name = newName;
     }
 
-    // REQUIRES: newBudget must be > 0
+    // REQUIRES: newBudget > 0
     // MODIFIES: this
-    // EFFECTS: sets the category budget equal to newBudget
+    // EFFECTS: sets the category budget
     public void setBudget(int newBudget) {
         this.budget = newBudget;
     }
 
-    // REQUIRES: an existing and valid category
-    // EFFECTS: adds the amounts of all transactions within the category and determines if the budget has been reached, 
-    // true if it has, false if not
+    // EFFECTS: returns true if the budget has been reached, false otherwise
     public Boolean isBudgetReached() {
-        int budgetCountTemporary = 0;
+        int totalSpent = 0;
         for (Transaction t : transactions.values()) {
-            budgetCountTemporary += t.getAmount();
+            totalSpent += t.getAmount();
         }
-        
-        return budgetCountTemporary >= this.getBudget();
+        return totalSpent >= this.getBudget();
     }
 
-    // GETTER METHODS
+    // Getter methods
 
     public int getBudget() {
         return this.budget;
-    } 
+    }
 
     public String getName() {
         return this.name;
@@ -60,5 +55,4 @@ public class Category {
     public HashMap<Integer, Transaction> getTransactions() {
         return this.transactions;
     }
-
 }
