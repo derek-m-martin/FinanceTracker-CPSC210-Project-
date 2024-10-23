80b8c6fd3e877e6ab3926158bd9910b6b728933c
derek-m-martin <derekmartin1005@gmail.com>
2024-10-23 16:58:53 -0700

    Implemented JsonReaderTest class

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,5 +1,56 @@
 package persistence;
 
-public class JsonReaderTest {
+import model.Category;
+import model.Transaction;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("ProjectStarter/data/nofile.json");
+        try {
+            HashMap<String, Category> categories = reader.readCategories();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyCategories() {
+        JsonReader reader = new JsonReader("ProjectStarter/data/testReaderEmptyCategories.json");
+        try {
+            HashMap<String, Category> categories = reader.readCategories();
+            assertEquals(5, categories.size());
+            assertEquals(0, categories.get("FOOD").getTransactions().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderNonEmptyCategories() {
+        JsonReader reader = new JsonReader("ProjectStarter/data/testReaderNonEmptyCategories.json");
+        try {
+            HashMap<String, Category> categories = reader.readCategories();
+            assertEquals(5, categories.size());
+
+            Category foodCategory = categories.get("FOOD");
+            assertNotNull(foodCategory);
+            assertEquals(900, foodCategory.getBudget());
+            assertEquals(1, foodCategory.getTransactions().size());
+
+            Transaction transaction = foodCategory.getTransactions().get(1);
+            checkTransaction(1, 100, "Grocery shopping", "FOOD", transaction);
+
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
