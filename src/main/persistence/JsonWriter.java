d90fcfed89df2f289efb349d88ac437c5fcbc1c9
derek-m-martin <derekmartin1005@gmail.com>
2024-10-23 17:10:07 -0700

    Implemented the JsonWriter class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,35 @@
 package persistence;
 
-public class JsonWriter {
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class JsonWriter {
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs a JsonWriter to write to the specified destination
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens the writer to begin writing to the destination file
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes the given JSON object to the file
+    public void write(JSONObject jsonObject) {
+        writer.print(jsonObject.toString());
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes the writer
+    public void close() {
+        writer.close();
+    }
 }
