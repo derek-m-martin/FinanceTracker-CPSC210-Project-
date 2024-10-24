15eb7dfef3142a947a3bba1157b53c0f75d98229
derek-m-martin <derekmartin1005@gmail.com>
2024-10-23 17:09:36 -0700

    Implemented the JsonTest class

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,5 +1,20 @@
 package persistence;
 
-public class JsonTest {
+import model.Category;
+import model.Transaction;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonTest {
+    protected void checkTransaction(int id, int amount, String description, String categoryName, Transaction transaction) {
+        assertEquals(id, transaction.getId());
+        assertEquals(amount, transaction.getAmount());
+        assertEquals(description, transaction.getDescription());
+        assertEquals(categoryName, transaction.getCategory().getName());
+    }
+
+    protected void checkCategory(String name, int budget, Category category) {
+        assertEquals(name, category.getName());
+        assertEquals(budget, category.getBudget());
+    }
 }
