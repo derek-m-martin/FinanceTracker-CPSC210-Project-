50868fc127ebbc7228d4693447621a3ac2a29423
derek-m-martin <derekmartin1005@gmail.com>
2024-11-21 12:44:16 -0800

    Added variables for the SummaryPanel class

--- src/main/ui/SummaryPanel.java
+++ src/main/ui/SummaryPanel.java
@@ -13,6 +13,12 @@ import java.util.List;
 // A panel to display spending summaries over a selected time period
 public class SummaryPanel extends JPanel {
 
+    private FinanceTrackerGUI mainApp;
+    private JTextField startDateField;
+    private JTextField endDateField;
+    private JButton generateButton;
+    private JTextArea summaryArea;
+
     // REQUIRES: mainApp is not null
     // EFFECTS: constructs the SummaryPanel
     public SummaryPanel(FinanceTrackerGUI mainApp) {
