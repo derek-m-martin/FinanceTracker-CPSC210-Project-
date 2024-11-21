9e79beb66ea2f981a092dcb9b36be04832d1c826
derek-m-martin <derekmartin1005@gmail.com>
2024-11-21 12:31:07 -0800

    Created the SummaryPanel class w/ specification

---
+++ src/main/ui/SummaryPanel.java
@@ -0,0 +1,38 @@
+package ui;
+
+import model.Category;
+import model.Transaction;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+// A panel to display spending summaries over a selected time period
+public class SummaryPanel extends JPanel {
+
+    // REQUIRES: mainApp is not null
+    // EFFECTS: constructs the SummaryPanel
+    public SummaryPanel(FinanceTrackerGUI mainApp) {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes and adds components to the panel
+    private void initializeComponents() {
+    }
+
+    // EFFECTS: generates and displays the spending summary
+    private void generateSummary() {
+    }
+
+    // EFFECTS: displays the spending summary in the text area
+    private void displaySummary(LocalDate startDate, LocalDate endDate,
+        int food, int housing, int entertainment, int transportation, int miscellaneous) {
+    }
+
+    // EFFECTS: retrieves transactions between the given dates
+    private List<Transaction> grabTransactions(LocalDate start, LocalDate end) {
+    }
+}
