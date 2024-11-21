740d8dce2cae32e25a2b16e4a75abd9bb38404ee
derek-m-martin <derekmartin1005@gmail.com>
2024-11-21 12:30:05 -0800

    Created the AddTransactionPanel class along with specification

---
+++ src/main/ui/AddTransactionPanel.java
@@ -0,0 +1,30 @@
+package ui;
+
+import model.Category;
+import model.Transaction;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// A panel that allows users to add new transactions
+public class AddTransactionPanel extends JPanel {
+
+    // REQUIRES: mainApp is not null
+    // EFFECTS: constructs the AddTransactionPanel
+    public AddTransactionPanel(FinanceTrackerGUI mainApp) {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: refreshes the categories in the combo box
+    public void refreshCategories() {
+    }
+
+    // Listener class for adding transactions
+    private class AddTransactionListener implements ActionListener {
+        // MODIFIES: mainApp
+        public void actionPerformed(ActionEvent e) {
+        }
+    }
+}
