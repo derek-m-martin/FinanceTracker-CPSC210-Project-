4c0a3ab8afd4d98c756a20b385f012e468aa8e7e
derek-m-martin <derekmartin1005@gmail.com>
2024-11-21 12:31:19 -0800

    Created the SetBudgetPanel w/ specification

---
+++ src/main/ui/SetBudgetPanel.java
@@ -0,0 +1,31 @@
+package ui;
+
+import model.Category;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// A panel to set budgets for categories
+public class SetBudgetPanel extends JPanel {
+
+    // REQUIRES: mainApp is not null
+    // EFFECTS: constructs the SetBudgetPanel
+    public SetBudgetPanel(FinanceTrackerGUI mainApp) {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes and adds components to the panel
+    private void initializeComponents() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: refreshes the categories in the combo box
+    public void refreshCategories() {
+    }
+
+    // Listener class for setting budgets
+    private class SetBudgetListener implements ActionListener {
+    }
+}
