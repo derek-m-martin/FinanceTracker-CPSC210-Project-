6e7b4f62e47f5fecf4ae945e4d42f949d956c33e
derek-m-martin <derekmartin1005@gmail.com>
2024-11-21 12:31:41 -0800

    Of course couldn't forget the image part of the phase by creating ImagePanel with specification

---
+++ src/main/ui/ImagePanel.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+// A custom panel to display an image (visual component)
+public class ImagePanel extends JPanel {
+    
+    // EFFECTS: loads the image to be displayed
+    public ImagePanel() {
+    }
+
+    // EFFECTS: paints the image onto the panel
+    protected void paintComponent(Graphics g) {
+    }
+}
